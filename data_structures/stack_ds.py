4a59d0b17eeacbbe7e972ca60e76fab751611fa2
Xero kun <elo48858@gmail.com>
2023-05-26 19:52:45 +0100

    `FEAT` Implemented stack data structure

---
+++ data_structures/stack_ds.py
@@ -0,0 +1,73 @@
+# github repository: https://github.com/dev-xero/python-algorithms-exercise-1.3
+
+"""stack_ds.py
+   Python implementation of a stack data structure
+"""
+
+# ---------------------------------------------------------------------------------------------------------
+
+
+from data_structures.linked_list_ds import Node
+
+
+# ---------------------------------------------------------------------------------------------------------
+
+
+class Stack:
+    """Stack implementation in Python"""
+    def __init__(self):
+        """Setup"""
+        self._first: Node | None = None
+        self._size: int = 0
+
+    def is_empty(self) -> bool:
+        return self._first is None
+
+    @property
+    def size(self) -> int:
+        return self._size
+
+    def push(self, item: str) -> None:
+        old_first = self._first
+        first = Node()
+
+        first.item = item
+        first.next = old_first
+
+        self._first = first
+        self._size += 1
+
+    def pop(self) -> str:
+        old_first = self._first
+        self._first = old_first.next
+
+        self._size -= 1
+        return old_first.item
+
+
+# ---------------------------------------------------------------------------------------------------------
+
+
+def main():
+    """Testing"""
+    test_stack = Stack()
+
+    print(test_stack.is_empty())
+    test_stack.push("this")
+    test_stack.push("is")
+    test_stack.push("a")
+    test_stack.push("stack")
+
+    print(test_stack.size)
+    print(test_stack.pop())
+    print(test_stack.pop())
+    print(test_stack.pop())
+
+    print(test_stack.size)
+
+
+# ---------------------------------------------------------------------------------------------------------
+
+
+if __name__ == "__main__":
+    main()
