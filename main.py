a45cfb23f3051d0a55bb6cd0cabc72b043093208
Xero kun <elo48858@gmail.com>
2023-05-26 19:10:24 +0100

    `FEAT` Initial project setup

---
+++ main.py
@@ -0,0 +1,19 @@
+# github repository: https://github.com/dev-xero/python-algorithms-exercise-1.3
+
+"""main.py
+   Python implementation of the exercises found in subchapter 1.3
+"""
+
+# ---------------------------------------------------------------------------------------------------------
+
+
+def main():
+    """Testing"""
+    print()
+
+
+# ---------------------------------------------------------------------------------------------------------
+
+
+if __name__ == "__main_-":
+    main()
