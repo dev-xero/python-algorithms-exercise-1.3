e6eebe15b57ce4589e1f10ee64bdb770f59ad486
Xero kun <elo48858@gmail.com>
2023-05-27 15:50:29 +0100

    `FEAT` Implemented algorithm to traverse and print the items in the node of a linked list

---
+++ algorithms/traverse_and_print.py
@@ -0,0 +1,56 @@
+# github repository: https://github.com/dev-xero/python-algorithms-exercise-1.3
+
+"""traverse_and_print.py
+   Python algorithm to traverse a linked list and print the items stored in the nodes
+"""
+
+# ---------------------------------------------------------------------------------------------------------
+
+
+from data_structures.linked_list_ds import Node
+
+
+# ---------------------------------------------------------------------------------------------------------
+
+
+def traverse_and_print(start: Node) -> None:
+    if not start or type(start) is not Node:
+        raise Exception("A valid node must be specified")
+
+    current = start
+
+    while current:
+        print(current.item)
+        current = current.next
+
+
+# ---------------------------------------------------------------------------------------------------------
+
+
+def main():
+    """Testing"""
+    first: Node = Node()
+    second: Node = Node()
+    third: Node = Node()
+    fourth: Node = Node()
+    fifth: Node = Node()
+
+    first.item = "first"
+    second.item = "second"
+    third.item = "third"
+    fourth.item = "fourth"
+    fifth.item = "fifth"
+
+    first.next = second
+    second.next = third
+    third.next = fourth
+    fourth.next = fifth
+
+    traverse_and_print(first)
+
+
+# ---------------------------------------------------------------------------------------------------------
+
+
+if __name__ == "__main__":
+    main()
