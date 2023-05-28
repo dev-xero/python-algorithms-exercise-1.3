de9b7a9315422026b0f8e9bf3835abd316d906d5
Xero kun <elo48858@gmail.com>
2023-05-28 11:23:42 +0100

    `FEAT` Implemented recursive algorithm to return the max key in a linked list

--- algorithms/linked_list_max.py
+++ algorithms/linked_list_max.py
@@ -34,17 +34,31 @@ def linked_list_max(head: Node) -> int:
 # ---------------------------------------------------------------------------------------------------------
 
 
+def recursive_linked_list_max(head: Node, max_item: int) -> int:
+    if head is None:
+        return max_item
+
+    if int(head.item) > max_item:
+        return recursive_linked_list_max(head.next, int(head.item))
+
+    return recursive_linked_list_max(head.next, max_item)
+
+
+# ---------------------------------------------------------------------------------------------------------
+
+
 def main():
     """Testing"""
-    head = Node("1")   # linked-list: 1 -> 2 -> 7 -> 4 -> 5
+    head = Node("1")   # linked-list: 1 -> 2 -> 7 -> 11 -> 10
     head.next = Node("2")
     head.next.next = Node("7")
-    head.next.next.next = Node("4")
-    head.next.next.next.next = Node("5")
+    head.next.next.next = Node("11")
+    head.next.next.next.next = Node("10")
 
     traverse_and_print(head)
     print()
     print("max:", linked_list_max(head))
+    print("recursive max:", recursive_linked_list_max(head, 0))
 
 
 # ---------------------------------------------------------------------------------------------------------
